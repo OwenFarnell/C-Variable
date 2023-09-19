a517669fa80fdd6a850c2cdf7355443ef6dba263
OwenFarnell <116113742+OwenFarnell@users.noreply.github.com>
2023-09-19 19:38:32 +0000

    fahr to celc

--- .ccls-cache/@home@runner@Test/main.cpp
+++ .ccls-cache/@home@runner@Test/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int classSize;
+// Function to convert Fahrenheit to Celsius
+double fahrenheitToCelsius(double fahrenheit) 
+{
+  return (fahrenheit - 32) * 5/9;
+}
 
-int main() {
-  classSize = 31;
-  cout << "The class has "<< classSize << " students.";
+int main() 
+{
+  double fahrenheit, celsius;
+  
+  cout << "Enter temperature in Fahrenheit: ";
+  cin >> fahrenheit;
+  
+  // Convert Fahrenheit to Celsius
+  celsius = fahrenheitToCelsius(fahrenheit);
+  
+  cout << "Temperature in Celsius: " << celsius << endl;
+  
   return 0;
 }
